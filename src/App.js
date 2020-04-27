d73c65bad7ac6d89bc73455cb11c487b275a8f2f
baihuihui <2223573875@qq.com>
2020-04-27 23:02:44 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -4,16 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-      App...
-      <Button block >点击我</Button>
-      <Button type="primary" >点击我旁边</Button>
-      <Button  ghost  >点击我</Button>
-        
-        
-      
-
-
-
+      App
+      <Button  >点击</Button>
       </div>
     )
   }
