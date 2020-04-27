b0d985edf44d42f65050539ec67c0d7b539b3754
baihuihui <2223573875@qq.com>
2020-04-27 22:53:18 +0800

    提交

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Button} from 'antd'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      App...
+      <Button block >点击我</Button>
+      <Button type="primary" >点击我旁边</Button>
+      <Button  ghost  >点击我</Button>
+        
+        
+      
+
+
+
+      </div>
+    )
+  }
+}
